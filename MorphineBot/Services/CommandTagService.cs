d379f9e78e5b4bbf46123a724517ce5d6c89ab7e
Hyblocker <bryan.spiteri@gmail.com>
2021-11-07 00:49:48 +0100

    Command Service init

---
+++ MorphineBot/Services/CommandTagService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace MorphineBot.Services
+{
+    /// <summary>
+    /// Commands in this context are essentially tags, as theyre a copy paste message with a known shorthand, shorthand being the command
+    /// </summary>
+    public class CommandTagService : IService
+    {
+        public async Task ProcessMessage(SocketCommandContext context)
+        {
+            
+        }
+    }
+}
