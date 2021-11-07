b9addc10250a3623867f8bb9ddb03f90baa40d24
Hyblocker <bryan.spiteri@gmail.com>
2021-11-07 09:48:47 +0100

    Enhance startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 git pull
 dotnet build -c Release
+# Using cd rather than just running dotnet because of working directory breaking where config files are loaded from,
+# and I'm too lazy to figure out how to run `dotnet` such that it also changes the working directory
 cd MorphineBot/bin/Release/net5.0
 dotnet MorphineBot.dll
