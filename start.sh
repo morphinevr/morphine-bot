c3cb4c290367671d4eea6d376da134cfc8be69de
Hyblocker <bryan.spiteri@gmail.com>
2021-11-24 01:26:48 +0100

    single instance start up script

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-git pull
-dotnet build -c Release
-# Using cd rather than just running dotnet because of working directory breaking where config files are loaded from,
-# and I'm too lazy to figure out how to run `dotnet` such that it also changes the working directory
-cd MorphineBot/bin/Release/net5.0
-dotnet MorphineBot.dll
+if pgrep -x "dotnet" >/dev/null
+then
+    echo "I'm already makin' piss!!!"
+else
+    git pull
+    dotnet build -c Release
+    # Using cd rather than just running dotnet because of working directory breaking where config files are loaded from,
+    # and I'm too lazy to figure out how to run `dotnet` such that it also changes the working directory
+    cd MorphineBot/bin/Release/net5.0
+    dotnet MorphineBot.dll
+fi
